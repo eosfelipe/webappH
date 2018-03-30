5ccdc3bfd7ef6a3fefb35a12de71eb8d895af8b9
eosfelipe <escobedo.felipe@hotmail.com>
2018-03-30 15:19:31 -0600

    img agregadas

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
-function mostrar(e){
+function siguiente(e){
   // var valor = document.getElementById('opc1');
   var pregunta = document.getElementsByClassName('input-hidden');
   // console.log(pregunta);
   console.log(e.target.value);
   // console.log(pregunta[0].value);
+  Reveal.navigateRight();
 }
-function verEvento(e){
+function iniciar(e){
   if(e.charCode == 13){
     e.preventDefault();
     var numOrden = document.getElementById('ordenServicio');
+    agregarLocal('numOrden',numOrden);
     console.log('HYUNDAI' + numOrden.value);
+    document.getElementById("atendiendo").innerHTML = obtenerLocal('numOrden');
     Reveal.navigateRight();
   }
   else{
     console.log('no es enter');
   }
 }
+
+function terminar(){
+  //post informacion a la BD
+  //...
+  //...
+  eliminarLocal('numOrden');
+}
+
+function agregarLocal(n,e){localStorage.setItem(n,'HYUNDAI'+e.value);}
+
+function eliminarLocal(e){localStorage.removeItem(e);}
+
+function obtenerLocal(e){return localStorage.getItem(e);}
