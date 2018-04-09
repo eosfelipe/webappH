f5c10a84859241127d43c9b8380090d65a25be4c
Felipe Escobedo <escobedo.felipe@hotmail.com>
2018-04-09 17:23:39 -0500

    pruebas php

--- accion.php
+++ accion.php
@@ -3,10 +3,17 @@ $numero = count($_POST);
 $tags = array_keys($_POST); // obtiene los nombres de las varibles
 $valores = array_values($_POST);// obtiene los valores de las varibles
 
+$ordenServicio = array();
+
 // crea las variables y les asigna el valor
 for($i=0;$i<$numero;$i++){
 $$tags[$i]=$valores[$i];
-echo json_encode($$tags[$i]);
+
+$ordenServicio.array_push($tags[$i] => $$tags[$i]);
+
+echo json_encode($ordenServicio);
+// echo json_encode($valores[$i]);
+// echo json_encode($$tags[$i]);
 }
 
 // if(isset($_POST['ordenServicio'])){
